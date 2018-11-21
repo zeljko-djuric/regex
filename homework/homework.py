256b7094f36feed42cdc10827d647418c2b06c00
Zeljko Djuric <zeljko94@gmail.com>
2018-11-21 22:09:54 +0100

    Processing data

--- homework/homework.py
+++ homework/homework.py
@@ -22,3 +22,25 @@ re_file = re.compile(r'^(\d)\.(java|pas|c|cpp)$')
 
 max_num_task = 0
 tasks = {}
+
+for f in os.listdir(homedir):
+    stud_dir = os.path.join(homedir, f)
+    m = re_dir.match(f)
+    if os.path.isdir(stud_dir) and m is not None and f in students:
+        student_id = m.group()
+        for sf in os.listdir(stud_dir):
+            task = os.path.join(stud_dir, sf)
+            m = re_file.match(sf)
+            if(os.path.isfile(task)) and m is not None:
+                task_t = int(m.group(1))
+                tasks[(student_id, task_t)] = m.group(2)
+                if(task_t > max_num_task):
+                    max_num_task = task_t
+
+for student_id, name in students.items():
+    print("\nName:" + name)
+    for i in range(1, max_num_task):
+        if(student_id, i) in tasks:
+            print("\t" + str(i) + ". " + tasks[(student_id,i)])
+        else:
+            print("\t"+ str(i) + ". -")
