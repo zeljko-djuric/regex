cf05f9ad08a27874dcc1aba1aaa0eca3c7019933
zeljko-djuric <zeljko94@gmail.com>
2018-11-21 19:10:57 +0100

    Added demo

---
+++ demo.py
@@ -0,0 +1,23 @@
+import sys
+import re
+
+if len(sys.argv) != 2:
+    sys.exit("python3 demo.py file_path")
+
+if re.match(r'.+\.txt',sys.argv[1],re.I) is None:
+    sys.exit("Extension must be .txt")
+
+re = ri.compile(r'(?i)(\b[A-Za-z]+\b)\s+\1')
+
+try:
+    f = open(sys.argv[1],"r")
+except IOError:
+    sys.exit("File open failed!")
+
+for line in f:
+    match = ri.search(line)
+    if match is not None:
+        print("Matching: %s\t groups: %s"%(match.group(), match.groups()))
+        print("Line without matching: " + ri.sub('anything', line))
+    
+f.close()
