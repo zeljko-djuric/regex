ea4f86d29358377e83a2628bc6c5b2a211f220c7
Zeljko Djuric <zeljko94@gmail.com>
2018-11-22 14:23:53 +0100

    Added regular expression for matching data in html files

--- exam/exam.py
+++ exam/exam.py
@@ -11,3 +11,13 @@ try:
 except IOError:
     sys.exit("File open failed")
 
+students = []
+points = []
+
+re_data = re.compile(r'<tr>'+
+r'\s*<td>\s*(?P<name>[A-Za-z]+( [A-Za-z]+)+)\s*</td>'+
+r'\s*<td>\s*(?P<practical>\d|[1-9]\d|100)</td>'+
+r'\s*<td>\s*(?P<theory>\d[1-9]\d|100)</td>'+
+r'\s*</tr>')
+
+m = re_data.search(data)
